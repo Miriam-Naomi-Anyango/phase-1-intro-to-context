8aae5aa9eb0d3cb92122312f9fd1bae105677424
Miriam Anyango <minaoanya@gmail.com>
2022-06-27 12:18:30 +0300

    commit tree 3f518079b4c677dcbc9f81389e4c68933bcd9426

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+function createEmployeeRecord(firstName, familyName, title, payPerHour){
+    return {
+        firstName:"Gray", 
+        familyName:"Worm", 
+        title: "Security",
+        payPerHour:1
+    }
+}
+console.log(createEmployeeRecord)
+
+
+function createEmployeeRecords(){
+    let employeeRecords;
+    let twoRows = [
+        ["moe", "sizlak", "barkeep", 2],
+        ["bartholomew", "simpson", "scamp", 3]
+      ]
+      
+}
